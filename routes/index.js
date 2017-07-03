0bc0f350331768d044a348229b0b23d9d0f2bbae
Nicholas Rohrer <nrohrer2@gmail.com>
2017-07-02 22:51:45 -0500

    API call working for unix times

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,24 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/:time', function(req, res, next){
-  var data = { time: req.params.time };
-  res.json(data);
+
+  function unixToNatural(unix) {
+    var date = new Date(unix * 1000);
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    var month = months[date.getMonth()];
+    var day = date.getDate();
+    var year = date.getFullYear();
+
+    return month + ' ' + day + ', ' + year;
+  }
+
+
+  if (!isNaN(req.params.time)) {
+    var result = unixToNatural(req.params.time);
+    var data = { unixDate: req.params.time, naturalDate: result };
+    res.json(data);
+  }
 });
 
 module.exports = router;
